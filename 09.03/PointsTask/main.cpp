0cd663370890574fce91c0ec3ef1a84f9de64b45
IvanFilipov <vanaka1189@gmail.com>
2016-03-13 12:48:38 +0200

    Update main.cpp
    comments added

--- 09.03/PointsTask/main.cpp
+++ 09.03/PointsTask/main.cpp
@@ -1,3 +1,6 @@
+//целта на задачата е да прочетем масив от точки от файл и да върнем индекса на най-близката 
+//до 0,0 точка
+
 #include<fstream>
 #include<iostream>
 
@@ -7,6 +10,7 @@ int main(){
 
 	std::ifstream Points("points.txt");
 
+//при неуспешно отваряне на файла ,програмата приключва
 	if (!Points.is_open())
 		return -1;
 
@@ -14,11 +18,13 @@ int main(){
 	int size;
 	Points >> size;
 
-
+//заделяме памет за нужния брой точки
 	Point* pntArr = new Point[size];
 
 	for (int i = 0; i < size; i++){
 
+//ако някоя операция с файловия поток пропадне 
+//почистваме заетата памет и прекратяваме изпълнението на програмата
 		if (!Points.good()){
 
 			delete[] pntArr;
@@ -26,11 +32,12 @@ int main(){
 			return -1;
 
 		}
-
+//прочитаме кооридинатите на всяка точка
 		Points >> pntArr[i].x >> pntArr[i].y;
 
 	}
 
+//извеждаме желаната информация ,почистваме и приключваме програмата
 	std::cout << "the index of the closest point is : "
 		<< ClosestPoint(pntArr, size);
 
